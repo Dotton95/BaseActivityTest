21d87ae8fc556faa880e83f856f934bb1a9a03d1
Dotton <dotton95@gmail.com>
2021-12-16 19:53:57 +0900

    기본 세팅

---
+++ app/src/main/java/com/example/baseactivitytest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.baseactivitytest
+
+import android.os.Bundle
+import com.example.baseactivitytest.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
